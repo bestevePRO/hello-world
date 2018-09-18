c3285ed7bc31a9d2b617fc64b48504523434b31c
unknown <besteve99@gmail.com>
2018-09-18 12:03:15 +0300

    Урок 11

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 showTimestamp = function(e) {
     var now = new Date();
     if (e.shiftKey) { // нажата кнопка CTRL
@@ -9,13 +8,3 @@ showTimestamp = function(e) {
         alert(date);
     }
 }
-=======
-(function () {
-  var button = document.querySelectorAll(".button")[0];
-  var rectangle = document.querySelectorAll(".rectangle")[0];
-  button.addEventListener("click", function () {
-    rectangle.style.borderColor = "green";
-    rectangle.style.backgroundColor = "red";
-  })
-})();
->>>>>>> c2998a3c6033f0e5838c296d6511fabb810d4a7a
